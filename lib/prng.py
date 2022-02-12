18377f76a8c1451c2bb1f83291a4df8ba656e082
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-02-12 00:42:41 -0600

    removed a magic number

--- lib/prng.py
+++ lib/prng.py
@@ -1,10 +1,14 @@
-def prng(mySeed):
-    from random import seed, random
+from random import seed, random
+
+def prng(mySeed, number_values):
     seed(mySeed)
     """
     pseudo-random number generator that takes a seed value
+    and returns a list of number_values values generated
+    from that seed
     """
-    return [random() for _ in range(10)]
+    return [random() for _ in range(number_values)]
 
 if __name__ == "__main__":
-    print(prng(12012))
+    prng(12012)
+
