9d7c8a7abd36e4080b1a57a59ff2b489de0ec8c1
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-02-21 10:55:26 -0600

    started working on gamecharacter class

---
+++ lib/gamecharacter.py
@@ -0,0 +1,19 @@
+import pygame as pg
+try:
+    from gameobject import GameObject
+    from config import SETTINGS
+except ModuleNotFoundError:
+    from .gameobject import GameObject
+    from .config import SETTINGS
+
+class GameCharacter(GameObject):
+    def __init__(self, pos: tuple[float], size: tuple[int], texture: str,
+                name: str):
+        """Initializes the object
+        pos: tuple of pixel locations for upper-left corner of object
+        size: tuple of pixels (width, height)
+        texture: texture path
+        name: name for the object
+        """
+        super().__init__(pos, size, texture)
+        self.name = name
