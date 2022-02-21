775881a83b3752225f9f211f6175b1dc86145cb5
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-02-21 10:55:04 -0600

    created gameobject class

---
+++ lib/gameobject.py
@@ -0,0 +1,26 @@
+try:
+    from config import SETTINGS
+except ModuleNotFoundError:
+    from .config import SETTINGS
+import pygame as pg
+
+class GameObject(pg.sprite.Sprite):
+    def __init__(self, pos: tuple[float], size: tuple[int], texture: str):
+        """Initializes the object
+        pos: tuple of pixel locations for upper-left corner of object
+        size: tuple of pixels (width, height)
+        texture: texture path
+        """
+        self.size = size
+        self.pos = pos
+
+        super().__init__()
+        with open(texture, 'r') as texture:
+            self.image = pg.image.load(texture).convert_alpha()
+        self.image = pg.transform.scale(self.image, self.size)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.pos
+
+    def draw(self, world: pg.Surface):
+        """Draws object to the world surface"""
+        world.blit(self.image, self.rect)
