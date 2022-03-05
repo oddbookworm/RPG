95d1886c0ae83dc27f94e44650b7a52fbd63ed76
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-03-05 09:52:54 -0600

    moved a docstring around

--- lib/prng.py
+++ lib/prng.py
@@ -1,12 +1,11 @@
 from random import seed, random
 
 def prng(mySeed, number_values):
-    seed(mySeed)
-    """
-    pseudo-random number generator that takes a seed value
+    """pseudo-random number generator that takes a seed value
     and returns a list of number_values values generated
     from that seed
     """
+    seed(mySeed)
     return [random() for _ in range(number_values)]
 
 if __name__ == "__main__":
