881415f6da055e1e8a4df14eb323463ab0827e28
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-03-05 09:50:01 -0600

    modified:   lib/errors.py
    Added an error for tilesets

--- lib/errors.py
+++ lib/errors.py
@@ -1,5 +1,11 @@
 class TupleAdditionError(Exception):
     """Raised when add_tuples runs into a problem, such as different size
-    tuples or tuples with different datatypes in the same index
+    tuples or tuples with different datatypes in the same index.
     """
+    pass
+
+class TilesetKeyError(Exception):
+    """Raised when a tileset has incorrect keys when passed into a method
+    that requires specific keys. To see the correct keys, look at the
+    docstring for that particular method."""
     pass
